65f02fe5ab74e33754a576e95a710dcd2a1f8513
mger1972 <mark@gerlach5.com>
2020-04-10 20:20:02 -0700

    Update ucRibbonControl.ascx.cs

--- SiteTest_Frames/controls/ucRibbonControl.ascx.cs
+++ SiteTest_Frames/controls/ucRibbonControl.ascx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevExpress.Web;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,15 @@ namespace SiteTest_Frames.controls
 {
 	public partial class ucRibbonControl : DevExpress.Web.ASPxRibbon
 	{
+		//private ;
+
 		protected void Page_Init(object sender, EventArgs e)
 		{
+			// When the button is created, set up some of the properties
+			this.Tabs.Clear();
+			this.Tabs.Add(new RibbonTab("_Base", "_Base"));
 
+			// Add in some of the buttons depending on the type
 		}
 
 		protected void Page_Load(object sender, EventArgs e)
